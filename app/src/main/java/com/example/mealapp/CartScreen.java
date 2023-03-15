eeb320130a64d7ae4a9ce4131271a5f6777b33af
JackFiorot <jmfiorot@lakeheadu.ca>
2023-03-14 20:27:48 -0400

    first cart page creation so cool

---
+++ app/src/main/java/com/example/mealapp/CartScreen.java
@@ -0,0 +1,14 @@
+package com.example.mealapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_screen);
+    }
+}
