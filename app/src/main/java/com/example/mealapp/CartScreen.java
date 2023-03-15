95e74921d856a331c7c805cf50946015cb4bca95
JackFiorot <jmfiorot@lakeheadu.ca>
2023-03-14 21:34:34 -0400

    cart page with 3 text boxes 3rd one should scroll

--- app/src/main/java/com/example/mealapp/CartScreen.java
+++ app/src/main/java/com/example/mealapp/CartScreen.java
@@ -3,6 +3,8 @@ package com.example.mealapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class CartScreen extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class CartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_screen);
+
+        TextView cartTextView = findViewById(R.id.firsttextbox3);
+
+        cartTextView.setMovementMethod(new ScrollingMovementMethod());
+        cartTextView.setText("jfvnsajnfjewanbgvjhdfbvhjdafbvjhdbvb jhdfbv hjdfbnvjkdfbhverijbv renvhuisldhnvnuildafhnvarduivhn sriv ndxjkdv bdfv iuvgreuigrfduivfduvjkcbvdfbvdfjkbnvjkdafnbfjkdhnb dfijbn dfkjnbfdjkv cjkn vvjkcn vmnv  nb  jkfnkjszndjknbbv hildfbv uirevuirefvburihncviudfsbnvi hbdfhvjbcdjkbvnfdkjahavn kjdfhnbv ijfald bnviuaedfebi");
+
     }
 }
